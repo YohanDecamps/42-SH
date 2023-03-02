fa82fc04190e8787025775be9b4f4d6ee4dfc695
baptiste0928 <22115890+baptiste0928@users.noreply.github.com>
2023-03-02 11:41:35 +0100

    feat: improve env error handling

--- src/builtin/env.c
+++ src/builtin/env.c
@@ -13,7 +13,15 @@
 
 int builtin_env(sh_command_t *command, sh_env_t *env)
 {
-    (void) command;
+    size_t args_len = mem_array_len(command->args) - 1;
+
+    if (args_len > 0) {
+        write(STDERR, "env: '", 6);
+        write(STDERR, command->args[1], str_len(command->args[1]));
+        write(STDERR, "': No such file or directory\n", 29);
+        return 127;
+    }
+
     for (size_t i = 0; i < env->env_size; i++) {
         sh_env_kv_t var = env->env[i];
         if (var.key == NULL) continue;
@@ -26,6 +34,17 @@ int builtin_env(sh_command_t *command, sh_env_t *env)
     return 0;
 }
 
+static bool is_alphanumeric(char c)
+{
+    if (c >= '0' && c <= '9')
+        return true;
+    if (c >= 'A' && c <= 'Z')
+        return true;
+    if (c >= 'a' && c <= 'z')
+        return true;
+    return false;
+}
+
 int builtin_setenv(sh_command_t *command, sh_env_t *env)
 {
     size_t args_len = mem_array_len(command->args) - 1;
@@ -35,10 +54,17 @@ int builtin_setenv(sh_command_t *command, sh_env_t *env)
         write(STDERR, "setenv: Too many arguments.\n", 28);
         return 1;
     }
+    size_t key_len = str_len(command->args[1]);
+    for (size_t i = 0; i < key_len; i++) {
+        if (!is_alphanumeric(command->args[1][i])) {
+            write(STDERR, "setenv: Variable name must contain", 34);
+            write(STDERR, " alphanumeric characters.\n", 26);
+            return 1;
+        }
+    }
 
     char *key = command->args[1];
     char *value = args_len == 2 ? command->args[2] : "";
-
     sh_env_set(env, key, value);
     return 0;
 }
